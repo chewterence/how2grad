2edd9eea2905914193065cd04b2d3e679194f33b
Terence Chew <chewterence@hotmail.com>
2020-06-05 15:33:44 +0800

    updated basic random plan

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -44,17 +44,17 @@ export default {
     return {
       // Testing preloaded planned data (COMMENT THESE OUT FOR CLEAN SLATE)
       // Semester 1
-      y1s1: ['CS1231', 'CS1010', 'ES1103', 'UTC1117', 'MA1521'],
-      y1s2: ['CS2030', 'CS2100', 'CS2040', 'UTW1001Z', 'UTC2101'],
+      y1s1: ['CS1231S', 'CS1010', 'ES1103', 'UTC1117', 'MA1521'],
+      y1s2: ['CS2030S', 'CS2100', 'CS2040S', 'UTW1001Z', 'UTC2101'],
       // Semester 2
-      y2s1: ['MA1101R', 'CS2103', 'CS2106', 'CS2101', 'UTC2105'],
+      y2s1: ['MA1101R', 'CS2103T', 'CS2106', 'CS2101', 'UTC2105'],
       y2s2: ['ST2334', 'ES2660', 'CS3230', 'UTW2001Q', 'CS2105'],
       // Semester 3
-      y3s1: ['', '', '', '', ''],
-      y3s2: ['', '', '', '', ''],
+      y3s1: ['CS2104', 'CS2107', 'CS3210', 'CS3220', 'CS3223'],
+      y3s2: ['CS3233', 'CS3234', 'CS3244', 'CS3241', 'CS3236'],
       // Semester 4
-      y4s1: ['', '', '', '', ''],
-      y4s2: []
+      y4s1: ['CS4212', 'CS4226', 'CS5224', 'CS5223', 'CS6207'],
+      y4s2: ['CS4225', 'CS4234', 'CS6270', 'CS6285', 'CS6234']
     }
   }
 }
